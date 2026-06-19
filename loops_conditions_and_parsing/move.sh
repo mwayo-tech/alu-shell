e1e0500992e9ec7df6685062f7ddee193b187c2c
mwayo-tech <m.wayo@alustudent.com>
2026-06-19 13:44:49 -0700

    done

---
+++ loops_conditions_and_parsing/move.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+mv 1-for_best_school \
+2-while_best_school \
+3-until_best_school \
+4-if_9_say_hi \
+5-4_bad_luck_8_is_your_chance \
+6-superstitious_numbers \
+7-clock \
+8-for_ls \
+9-to_file_or_not_to_file \
+10-fizzbuzz \
+11-read_and_cut \
+12-tell_the_story_of_passwd \
+13-lets_parse_apache_logs \
+14-dig_the-data \
+loops_conditions_and_parsing/
